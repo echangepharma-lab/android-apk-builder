665f7670e1e717d6a60a678eb4a26bde2fda8a6f
echangepharma-lab <echangepharma@gmail.com>
2026-08-14 19:58:57 +0100

    Add files via upload

---
+++ app/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.mediapp.interactions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mediapp.interactions.data.remote.BarcodeResolver
+import com.mediapp.interactions.ui.screens.AjoutMedicamentScreen
+import com.mediapp.interactions.ui.screens.HomeScreen
+import com.mediapp.interactions.ui.screens.ScannerScreen
+import com.mediapp.interactions.ui.theme.MediAppTheme
+import com.mediapp.interactions.ui.viewmodel.MedicamentViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var barcodeResolver: BarcodeResolver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MediAppTheme {
+                MaterialTheme {
+                    val navController = rememberNavController()
+                    val viewModel: MedicamentViewModel = hiltViewModel()
+                    NavHost(navController = navController, startDestination = "home") {
+                        composable("home") {
+                            HomeScreen(
+                                viewModel = viewModel,
+                                onNavigateToAjout = { navController.navigate("ajout") }
+                            )
+                        }
+                        composable("ajout") { entry ->
+                            AjoutMedicamentScreen(
+                                barcodeResolver = barcodeResolver,
+                                backStackEntry = entry,
+                                onNavigateToScanner = { navController.navigate("scanner") },
+                                onNavigateBack = { navController.popBackStack() },
+                                viewModel = viewModel
+                            )
+                        }
+                        composable("scanner") {
+                            ScannerScreen(
+                                onBarcodeScanned = { code ->
+                                    navController.previousBackStackEntry
+                                        ?.savedStateHandle
+                                        ?.set("scanned_barcode", code)
+                                    navController.popBackStack()
+                                },
+                                onNavigateBack = { navController.popBackStack() }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
