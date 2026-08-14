d2f70fb9d90f6471c8ac2e5483b4c6ccf4d1225d
echangepharma-lab <echangepharma@gmail.com>
2026-08-14 16:40:56 +0100

    Add files via upload

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -1,28 +1,66 @@
-package com.example.sampleapp
+package com.mediapp.interactions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mediapp.interactions.data.remote.BarcodeResolver
+import com.mediapp.interactions.ui.screens.AjoutMedicamentScreen
+import com.mediapp.interactions.ui.screens.HomeScreen
+import com.mediapp.interactions.ui.screens.ScannerScreen
+import com.mediapp.interactions.ui.theme.MediAppTheme
+import com.mediapp.interactions.ui.viewmodel.MedicamentViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
-    private var clickCount = 0
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var barcodeResolver: BarcodeResolver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val welcomeText = findViewById<TextView>(R.id.welcomeText)
-        val clickButton = findViewById<Button>(R.id.clickButton)
-        val countText = findViewById<TextView>(R.id.countText)
-
-        clickButton.setOnClickListener {
-            clickCount++
-            countText.text = "Button clicked $clickCount times"
-            
-            if (clickCount % 5 == 0) {
-                Toast.makeText(this, "Wow! You've clicked $clickCount times!", Toast.LENGTH_SHORT).show()
+        enableEdgeToEdge()
+        setContent {
+            MediAppTheme {
+                MaterialTheme {
+                    val navController = rememberNavController()
+                    val viewModel: MedicamentViewModel = hiltViewModel()
+                    NavHost(navController = navController, startDestination = "home") {
+                        composable("home") {
+                            HomeScreen(
+                                viewModel = viewModel,
+                                onNavigateToAjout = { navController.navigate("ajout") }
+                            )
+                        }
+                        composable("ajout") { entry ->
+                            AjoutMedicamentScreen(
+                                barcodeResolver = barcodeResolver,
+                                backStackEntry = entry,
+                                onNavigateToScanner = { navController.navigate("scanner") },
+                                onNavigateBack = { navController.popBackStack() },
+                                viewModel = viewModel
+                            )
+                        }
+                        composable("scanner") {
+                            ScannerScreen(
+                                onBarcodeScanned = { code ->
+                                    navController.previousBackStackEntry
+                                        ?.savedStateHandle
+                                        ?.set("scanned_barcode", code)
+                                    navController.popBackStack()
+                                },
+                                onNavigateBack = { navController.popBackStack() }
+                            )
+                        }
+                    }
+                }
             }
         }
     }
